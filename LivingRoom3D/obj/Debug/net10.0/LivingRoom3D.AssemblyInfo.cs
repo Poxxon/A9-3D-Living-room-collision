b9eaea8b80ac689ba149dd6d7cb2dee823c7d271
Pouya Rad <pouyarad15@gmail.com>
2025-11-26 23:26:49 -0500

    first run after commit (successful)

--- LivingRoom3D/obj/Debug/net10.0/LivingRoom3D.AssemblyInfo.cs
+++ LivingRoom3D/obj/Debug/net10.0/LivingRoom3D.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LivingRoom3D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a1bb0523893418ff96843a045841bfd48f4b6b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LivingRoom3D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LivingRoom3D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
